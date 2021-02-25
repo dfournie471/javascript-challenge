ed7e5ea70aa78aa61f37229c687ab96264a1f6b9
David Fournie <drfournie@gmail.com>
2021-02-24 18:02:56 -0600

    forms convert text to lowercase. added comments

--- UFO-level-1_&_2/static/js/app.js
+++ UFO-level-1_&_2/static/js/app.js
@@ -5,28 +5,40 @@ var tableData = data;
 // YOUR CODE HERE!
 
 
+//create referemce to table body
 var tbody=d3.select("tbody");
+//loop through each element of data and add a tr for each element
 tableData.forEach((alienReport) => {
     var row =tbody.append("tr");
+    //select the key value pairs in each element
     Object.entries(alienReport).forEach(([key,value]) => {
+        //add td for each key value pair
         var cell = row.append("td");
+        //fill cell with dict value
         cell.text(value);
     });
 
 });
 
+//reference classes for listener events
 var button =d3.select("#filter-btn");
-var form =d3.select("form");
+// var form =d3.select("form");
 var resetButton=d3.select("#reset-btn");
+
+
+//create listener events
+// form.on("submit", runEnter);
 button.on("click", runEnter);
-form.on("submit", runEnter);
 resetButton.on("click", runReset);
 
 
 
 
+//create runEnter fuction that will run after event is executed
 function runEnter(){
+    //prevent page from refreshing
     d3.event.preventDefault();
+    //create variables that select id of forms
     var inputElementDate= d3.select("#datetime");
     var inputElementCity=d3.select("#city");
     var inputElementState=d3.select("#state");
@@ -34,13 +46,16 @@ function runEnter(){
     var inputElementShape=d3.select("#shape");
     
 
+    //create variables that get the values entered into forms
+    // lowercase relevant forms
     var inputValueDate=inputElementDate.property("value");
-    var inputValueCity= inputElementCity.property("value");
-    var inputValueState= inputElementState.property("value");
-    var inputValueCountry= inputElementCountry.property("value");
-    var inputValueShape= inputElementShape.property("value");
+    var inputValueCity= inputElementCity.property("value").toLowerCase();
+    var inputValueState= inputElementState.property("value").toLowerCase();
+    var inputValueCountry= inputElementCountry.property("value").toLowerCase();
+    var inputValueShape= inputElementShape.property("value").toLowerCase();
 
 
+    //create if statements to check if forms are empty.
     if ( inputValueDate !=""){
     tableData= tableData.filter(report => report.datetime === inputValueDate);
     if (inputValueCity !=""){
@@ -66,7 +81,8 @@ function runEnter(){
         });
     });
     
-   
+    
+    // repeat for each form
     } else if(inputValueCity !=""){
         tableData=tableData.filter(report => report.city === inputValueCity);
         
@@ -144,18 +160,11 @@ function runEnter(){
     
    
     }
-   
-    
-   
-    
-    
-    return;
+return;
 
 }
+//create function that resets table
 function runReset(){
-    
     location.reload(true);
-    
-    
 }
 
